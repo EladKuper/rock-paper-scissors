0f9896c677c7762ef90867c53eb033a8e200f1f3
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-03-30 18:35:33 +0300

    Create a function that gets user choice of play and changes it to lower-case

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function getComputerChoice() {
     let random = Math.floor( Math.random() * playOptions.length);
     return playOptions[random];
 }
+
+function playARound() {
+    let input = prompt("Please choose an option between 'Rock', 'Paper', and 'Scissors'", "Rock");
+    let playerSelection = input.toLowerCase();
+    console.log(input);
+    console.log(playerSelection);
+}
+
+console.log(playARound());
