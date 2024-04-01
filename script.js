f267098e7ccb48adddeccef8e3a98cad83f91d01
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-04-01 15:02:11 +0300

    Finish func playRound and add func playerWonOrLost

--- script.js
+++ script.js
@@ -4,10 +4,45 @@ function getComputerChoice() {
     return playOptions[random];
 }
 
-function playRound() {
-    let input = prompt("Please choose an option between 'Rock', 'Paper', and 'Scissors'", "Rock");
-    let playerSelection = input.toLowerCase();
-    let computerSelection = getComputerChoice();
-    
+function capitalFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function playerWonOrLost(player, computer) {
+    if (player === "rock") {
+        if (computer === "scissors") return "won";
+        else if (computer === "paper") return "lost";
+        else return "tie";
+    }
+    else if (player === "paper") {
+        if (computer === "rock") return "won";
+        else if (computer === "scissors") return "lost";
+        else return "tie";
+    }
+    else if (player === "scissors") {
+        if (computer === "paper") return "won";
+        else if (computer === "rock") return "lost";
+        else return "tie";
+    }
+    else return false;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let input = prompt("Please choose an option between 'Rock', 'Paper', and 'Scissors'", "Rock");
+    playerSelection = input.toLowerCase();
+    computerSelection = getComputerChoice();
+
+    let playerIsWinner = playerWonOrLost(playerSelection, computerSelection);
+    if (playerIsWinner === "won") {
+        return "You won! " +playerSelection+ " beats " +computerSelection; 
+    }
+    else if (playerIsWinner === "lost") {
+        return "You lost! " +computerSelection+ " beats " + playerSelection;
+    }
+    else if (playerIsWinner === "tie") {
+        return "It's a tie!"
+    }
+    else return "Check your spelling"
+}
+
+console.log(playRound());
